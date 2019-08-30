2d1119d2999b60d4241189f7151e596826fa5f98
Elias Jackson <ejthedj0@gmail.com>
2019-08-30 00:59:00 -0700

    First attempt at YouTube search

--- functions/commands.js
+++ functions/commands.js
@@ -1,8 +1,19 @@
+const ytsearch = require('youtube-search');
+
 module.exports = {
-  share: (msg, query) => {
-    apikey = process.env.YOUTUBE_API_KEY;
-  
-    msg.reply(`you searched for ${query}`);
+  share: async (query) => {
+    let opts = {
+      maxResults: 10,
+      key: process.env.YOUTUBE_API_KEY
+    };
+
+    return ytsearch(query, opts)
+    .then(result => {
+      console.log(result);
+      return 'Search success';
+    });
+    
+    // msg.reply(`you searched for ${query}`);
     // add emoticons
   }
 }
--- message.js
+++ message.js
@@ -2,6 +2,17 @@ const commands = require('./functions/commands');
 
 module.exports = msg => {
   if(msg.content.startsWith('$share')) {
-    commands.share(msg, msg.content.substr(7));
+    commands.share(msg, msg.content.substr(7))
+    .then(replymsg => {
+      msg.reply(replymsg)
+      .then(newMsg => {
+        newMsg.react('🤘');
+        newMsg.react('👎');
+      })
+    })
+    .catch(error => {
+      console.log(error);
+      msg.reply('Search error');
+    })
   }
 }
